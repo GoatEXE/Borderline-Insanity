d39f9a34ce92290f2a12516cd05e6b9e6adb23f1
GoatEXE <DTCoppock@gmail.com>
2024-08-16 15:58:57 -0500

    Added Player Bullets
    Bullets are spawned by clicking within the game, but I plan on putting this on a timer. The bullet then continues till it goes off screen then removes itself from the queue.

--- Scripts/Player/Player.cs
+++ Scripts/Player/Player.cs
@@ -2,6 +2,7 @@ using Godot;
 
 public partial class Player : Area2D
 {
+	[Export] public PackedScene BulletScene;
 	[Export] public int Speed { get; set; } = 400;
 	public int HalfSpriteSize { get; set; } = 33;
 	public Main MainScreen { get; set; }
@@ -19,5 +20,23 @@ public partial class Player : Area2D
 		Position = position;
 		Show();
 		GetNode<CollisionShape2D>("CollisionShape2D").Disabled = false;
+	}
+
+	public override void _UnhandledInput(InputEvent @event)
+	{
+		if (@event is InputEventMouseButton eventMouseButton && eventMouseButton.Pressed)
+		{
+			if (eventMouseButton.ButtonIndex == MouseButton.Left)
+			{
+				SpawnBullet();
+			}
+		}
+	}
+
+	private void SpawnBullet()
+	{
+		var bullet = (PlayerBullet)BulletScene.Instantiate();
+		bullet.GlobalPosition = GlobalPosition;
+		GetParent().AddChild(bullet);
 	}
 }
---
+++ Scripts/Player/PlayerBullet.cs
@@ -0,0 +1,39 @@
+using Godot;
+
+public partial class PlayerBullet : Area2D
+{
+	[Export] public int Speed = 800;
+
+	private Vector2 _direction;
+
+	public override void _Ready()
+	{
+		// Where's the mouse at?
+		Vector2 targetPosition = GetGlobalMousePosition();
+
+		// Calculate the direction to the mouse
+		_direction = (targetPosition - GlobalPosition).Normalized();  
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		// Move the bullet in the direction calculated
+		Vector2 velocity = _direction * Speed * (float)delta;
+		GlobalPosition += velocity;
+
+		// Check if the bullet is outside the screen
+		if (!IsOnScreen())
+		{
+			QueueFree();
+		}
+	}
+
+	private bool IsOnScreen()
+	{
+		// Get the screen bounds
+		Rect2 screenRect = new Rect2(Vector2.Zero, GetViewportRect().Size);
+
+		// Check if the bullet is within the screen bounds
+		return screenRect.HasPoint(GlobalPosition);
+	}
+}
